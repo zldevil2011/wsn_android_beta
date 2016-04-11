7fba4e5c16d9ae94b71c30a2bb31b515da4cd142
zldevil2011 <929034478@qq.com>
2016-04-11 11:27:25 +0800

    taking phoyo

--- app/src/main/java/com/newly_dawn/app/wsn/MainActivity.java
+++ app/src/main/java/com/newly_dawn/app/wsn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.newly_dawn.app.wsn;
 
 import android.content.Intent;
+import android.graphics.CornerPathEffect;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -24,19 +25,21 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private int current_menu = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        build();
+        build_page();
+        buidl_drawer();
     }
-    public void build(){
+    public void build_page(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,7 +51,9 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+    }
+    public void buidl_drawer(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -95,34 +100,87 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        FrameLayout fl = (FrameLayout)findViewById(R.id.content_frame);
-        LayoutInflater factorys = LayoutInflater.from(MainActivity.this);
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            final View takingPhoto_view = factorys.inflate(R.layout.activity_taking_photo, null);
-            CoordinatorLayout rl1 = (CoordinatorLayout)takingPhoto_view.findViewById(R.id.activity_taking_photo_page);
-            CoordinatorLayout rl2 = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
-            fl.removeView(rl2);
-            fl.addView(rl1);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-            final View main_view = factorys.inflate(R.layout.app_bar_main, null);
-            CoordinatorLayout rl1 = (CoordinatorLayout)main_view.findViewById(R.id.app_bar_main_page);
-            final View setting_view = factorys.inflate(R.layout.activity_setting, null);
-            CoordinatorLayout rl2 = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
-            fl.removeView(rl2);
-            fl.addView(rl1);
-
-        } else if (id == R.id.nav_manage) {
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        FrameLayout content_frame = (FrameLayout)findViewById(R.id.content_frame);
+        int pre_current_menu = current_menu;
+        CoordinatorLayout preLayout = getPreLayout();
+        CoordinatorLayout nextLayout = getNextLayout(id);
+        if(preLayout != null && nextLayout != null) {
+            content_frame.removeView(preLayout);
+            content_frame.addView(nextLayout);
+            TakingPhoto tk = new TakingPhoto();
+            tk.build(LayoutInflater.from(MainActivity.this), MainActivity.this);
+            build_page();
+        }else{
+            Toast.makeText(MainActivity.this, "敬请期待", Toast.LENGTH_SHORT).show();
+            current_menu = pre_current_menu;
         }
-        build();
+        buidl_drawer();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public CoordinatorLayout getPreLayout(){
+        CoordinatorLayout preLayout = null;
+        switch (current_menu){
+            case 0:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_index_page);
+                break;
+            case 1:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_taking_photo_page);
+                break;
+            case 2:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
+                break;
+            case 3:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
+                break;
+            case 4:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
+                break;
+            case 5:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
+                break;
+            case 6:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_setting_page);
+                break;
+            default:
+                preLayout = (CoordinatorLayout)findViewById(R.id.activity_index_page);
+                break;
+        }
+        return preLayout;
+    }
+    public CoordinatorLayout getNextLayout(int id){
+        LayoutInflater factorys = LayoutInflater.from(MainActivity.this);
+        final View nextView;
+        CoordinatorLayout nextLayout = null;
+        if(id == R.id.nav_index && current_menu != 0){
+            // turn to the Home
+            nextView = factorys.inflate(R.layout.activity_index, null);
+            nextLayout = (CoordinatorLayout)nextView.findViewById(R.id.activity_index_page);
+            current_menu = 0;
+        }
+        else if (id == R.id.nav_camera && current_menu != 1) {
+            // Handle the camera action
+            nextView = factorys.inflate(R.layout.activity_taking_photo, null);
+            nextLayout = (CoordinatorLayout)nextView.findViewById(R.id.activity_taking_photo_page);
+            current_menu = 1;
+        } else if (id == R.id.nav_gallery && current_menu != 2) {
+            current_menu = 2;
+        } else if (id == R.id.nav_slideshow && current_menu != 3) {
+            current_menu = 3;
+        } else if (id == R.id.nav_manage && current_menu != 4) {
+            nextView = factorys.inflate(R.layout.activity_setting, null);
+            nextLayout = (CoordinatorLayout)nextView.findViewById(R.id.activity_setting_page);
+            current_menu = 4;
+        } else if (id == R.id.nav_share && current_menu != 5) {
+            current_menu = 5;
+        } else if (id == R.id.nav_send && current_menu != 6) {
+            current_menu = 6;
+        }
+        return nextLayout;
+    }
+    public void update_listener(int id){
+        TakingPhoto tk = new TakingPhoto();
+        tk.build(LayoutInflater.from(MainActivity.this), MainActivity.this);
     }
 }
--- app/src/main/java/com/newly_dawn/app/wsn/TakingPhoto.java
+++ app/src/main/java/com/newly_dawn/app/wsn/TakingPhoto.java
@@ -1,29 +1,76 @@
 package com.newly_dawn.app.wsn;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 
-public class TakingPhoto extends AppCompatActivity {
+import java.io.IOException;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_taking_photo);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+public class TakingPhoto{
+    private Camera camera; // 相机对象
+    public void build(LayoutInflater factorys, AppCompatActivity context){
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final View nextView;
+        Log.i("zl_debug", "TAKINGPHOTO");
+        Toolbar toolbar = (Toolbar) context.findViewById(R.id.toolbar);
+        context.setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) context.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "TAKEING Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        listener(context);
     }
+    public void listener(AppCompatActivity context){
+        Button takePhoto = (Button)context.findViewById(R.id.takephoto); // 获取“预览”按钮
+        SurfaceView sv = (SurfaceView)context.findViewById(R.id.surfaceView1); // 获取SurfaceView组件，用于显示相机预览
+        final SurfaceHolder sh = sv.getHolder();
+        sh.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS); // 设置该SurfaceHolder自己不维护缓冲
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 如果相机为非预览模式，则打开相机
 
+                    camera=Camera.open(); // 打开相机
+
+                try {
+                    camera.setDisplayOrientation(90);
+                    camera.setPreviewDisplay(sh); // 设置用于显示预览的SurfaceView
+                    Camera.Parameters parameters = camera.getParameters();	//获取相机参数
+                    parameters.setPictureSize(640, 480);	//设置预览画面的尺寸
+                    parameters.setPictureFormat(PixelFormat.JPEG);	//指定图片为JPEG图片
+                    parameters.set("jpeg-quality", 80);	//设置图片的质量
+                    parameters.setPictureSize(640, 480); 	//设置拍摄图片的尺寸
+                    camera.setParameters(parameters);	//重新设置相机参数
+                    camera.startPreview();	//开始预览
+                    camera.autoFocus(null); // 设置自动对焦
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    final Camera.PictureCallback jpeg = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+        }
+    };
 }
