7dc9e51701f703350ac363ffc791cdb334929cbd
zldevil2011 <929034478@qq.com>
2016-04-12 22:27:48 +0800

    get city_list and county_list

--- app/src/main/java/com/newly_dawn/app/wsn/Weather.java
+++ app/src/main/java/com/newly_dawn/app/wsn/Weather.java
@@ -3,6 +3,7 @@ package com.newly_dawn.app.wsn;
 import android.content.Context;
 import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +22,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Weather{
     private Spinner spinnerTopProvince = null;
     private Spinner spinnerSubCitys = null;
-    private String[][] citys ={{"朝阳" ,  "阳台" ,  "紫金" ,  "海淀"},
+    private Spinner spinnerSubCountys = null;
+    private ArrayList<City> cityCode = null;
+    private Set<String> provices = new HashSet<>();
+    private Map<String, HashSet> citys = new HashMap<>();
+    private Map<String, HashSet> countys = new HashMap<>();
+
+    private String[][] cityss ={{"朝阳" ,  "阳台" ,  "紫金" ,  "海淀"},
             { "抚顺" ,  "大连" ,  "青岛" ,  "烟台"},
             {"济南" ,  "菏泽" , "威海", "单县" },
             {"开封" ,  "安阳" ,  "洛阳" ,  "南阳"}};
@@ -50,13 +62,14 @@ public class Weather{
     }
     public void listener(final AppCompatActivity context){
         spinnerTopProvince=(Spinner)context.findViewById(R.id.Province);
-        spinnerSubCitys=(Spinner)context.findViewById(R.id.city);
+        spinnerSubCitys=(Spinner)context.findViewById(R.id.City);
+        spinnerSubCountys=(Spinner)context.findViewById(R.id.County);
         spinnerTopProvince.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Weather.this.arrayAdapter = new ArrayAdapter<CharSequence>(context,
-                        android.R.layout.simple_spinner_item, citys[position]);
-                spinnerSubCitys.setAdapter(arrayAdapter);
+//                Weather.this.arrayAdapter = new ArrayAdapter<CharSequence>(context,
+//                        android.R.layout.simple_spinner_item, citys[position]);
+//                spinnerSubCitys.setAdapter(arrayAdapter);
             }
 
             @Override
@@ -64,11 +77,53 @@ public class Weather{
 
             }
         });
-        ArrayList<City> ans = ParseXml(getXMLFromResXml());
-        for(int i = 0; i < ans.size(); ++i){
-            City tmp = ans.get(i);
+        readCityCode();
+        for(int i = 0; i < cityCode.size(); ++i){
+            City tmp = cityCode.get(i);
             Log.i("cityCode", "" +tmp.getProvice() + " " +tmp.getCity() + " " +tmp.getCounty() + " " + tmp.getID());
         }
+    }
+    public void readCityCode(){
+        cityCode = ParseXml(getXMLFromResXml());
+        getList();
+    }
+    public void getList(){
+        ArrayList<Set> zl = new ArrayList<>();
+        Map<String, Set> zll = new HashMap<>();
+        zll.put("a", new HashSet());
+        int len = cityCode.size();
+        for(int i = 0; i < len; ++i){
+            provices.add(cityCode.get(i).getProvice());
+        }
+        Log.i("provices_len", "" + provices.size());
+        int len_provice = provices.size();
+        int step = 0;
+        for(String provice : provices){
+            HashSet<String> tmp = new HashSet<String>();
+            for(step = 0; step < len; ++step){
+                if(cityCode.get(step).getProvice().equals(provice)){
+                    tmp.add(cityCode.get(step).getCity());
+                }
+            }
+            Log.i("_len__", provice + " " + tmp);
+            citys.put(provice, tmp);
+        }
+        Log.i("citys_len", "" + citys.size());
+        step = 0;
+        for(HashSet<String> value : citys.values()){
+
+            for(String city : value){
+                HashSet<String> tmp = new HashSet<>();
+                for(step = 0; step < len; ++step){
+                    if(cityCode.get(step).getCity().equals(city)){
+                        tmp.add(cityCode.get(step).getCounty());
+                    }
+                }
+                Log.i("_len_", city + " " + tmp);
+                countys.put(city, tmp);
+            }
+        }
+        Log.i("countys_len", "" + countys.size());
     }
     public ArrayList<City> ParseXml(XmlPullParser parser){
         ArrayList<City> CityArray = new ArrayList<City>();
@@ -93,12 +148,10 @@ public class Weather{
                     if(tagName.equals("province")){
                         CityTemp = new City();
                         String Provice = (parser.getAttributeValue(1));
-//                        Log.i("cityCode_p", Provice);
                         CityTemp.setProvice(Provice);
                         full = 1;
                     }else if(tagName.equals("city")){
                         String city = parser.getAttributeValue(1);
-//                        Log.i("cityCode_c", city);
                         String provice = CityTemp.getProvice();
                         CityTemp = new City();
                         CityTemp.setProvice(provice);
